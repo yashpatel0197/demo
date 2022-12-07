b95dfdd263f419f166669034b6b2d64540a191ec
yashpatel <yashpatel0197@gmail.com>
2022-12-07 12:38:41 +0530

    laravel first update

---
+++ app/Http/Controllers/Student.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Student extends Controller
+{
+    //
+    public function index(){
+        echo "hello i am student";
+    }
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,5 @@
+@extends("mytheam.template")
+@section("content")
+<h1>hello</h1>
+    
+@endsection
